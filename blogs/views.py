c95189bc332e2c18dd3ced0341c3b2ee8bb229f0
kripton017 <charles4949@mail.ru>
2022-07-25 14:12:17 +0500

    added colormodel, sizemodel, brandmodel

---
+++ blogs/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, reverse, get_object_or_404
+from django.views.generic import ListView, DetailView, CreateView
+from .models import PostModel
+from .forms import CommentModelForm
+
+
+class PostListView(ListView):
+    template_name = 'blog.html'
+
+    def get_queryset(self):
+        qs = PostModel.objects.order_by('-id')
+        tag = self.request.GET.get('tag')
+        if tag:
+            qs = qs.filter(tag__name=tag)
+        return qs
+
+
+class PostDetailView(DetailView):
+    model = PostModel
+    template_name = 'blog-detail.html'
+
+
+class CommentCreateView(CreateView):
+    form_class = CommentModelForm
+
+    def form_valid(self, form):
+        print(form.instance)
+        form.instance.post = get_object_or_404(PostModel, pk=self.kwargs.get('pk'))
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('blogs:detail', kwargs={'pk':self.kwargs.get('pk')})
